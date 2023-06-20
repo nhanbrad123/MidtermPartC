b8b8080ed6cad9ee6c6d9a744c687ef38b8771b8
Ly Trong Nhan <lytro@sheridancollege.ca>
2023-06-20 14:11:49 -0400

    First commit!

---
+++ src/midtermexam_versiona_extensioncode/TestUserProfile.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+/**
+ *
+ * @author Admin
+ */
+public class TestUserProfile {
+    public static void main(String[] args)
+    {
+        UserProfile a1 = new UserProfile("Ly Trong Nhan", "Drama");
+        System.out.printf("Name: %s\nGenre: %s", a1.getUserID(),a1.getGenre());
+        
+    }
+}
